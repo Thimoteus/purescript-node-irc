5bcb0f9a2d188f3a3c221a1afdb9a9bb87b1df9b
Thimoteus <thimoteus@teknik.io>
2015-08-16 13:54:33 +0200

    removed some console messages

--- src/Node/IRC/BareBones.js
+++ src/Node/IRC/BareBones.js
@@ -20,7 +20,6 @@ exports.clientMethod = function (method) {
       return function (ircCallback) {
         return function () {
           client[method](eventType, function() {
-            console.log('Got a ' + eventType + ' event!')
             var args = ircCallback.fromArgumentsJS(arguments)
             ircCallback.action(args)()
           })
@@ -35,7 +34,6 @@ exports.say = function (client) {
   return function (target) {
     return function (message) {
       return function () {
-        console.log('About to say "' + message + '" to ' + target)
         client.say(target, message)
       }
     }
